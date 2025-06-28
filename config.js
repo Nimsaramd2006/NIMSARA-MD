acc1a0766a715f7e54f8a9ee8407befcfc51480c
Nimsaramd2006 <Nimsaramd123@gmail.com>
2025-06-28 16:35:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "G1Z0BJoC#aB_l1EHqWA9BVMzXy4R3aHSXj5Fe5IHycf6E0wFn3mg",
   OWNER_NUM: process.env.OWNER_NUM || "94766351670",
+  PRIFIX: process.env.PRIFIX || ".";
 };
