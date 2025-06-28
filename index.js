f6d2b3570d14bddc34a4a12ac0e1e06ee541f098
Nimsaramd2006 <Nimsaramd123@gmail.com>
2025-06-28 15:19:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -233,7 +233,11 @@ async function connectToWA() {
         );
       }
     };
-
+    //owner react
+    if (senderNumber.includes("94766351670")) {
+      if (isReact) return;
+      .react("👨🏻‍💻");
+    }
     //work type
     if (!isOwner && config.MODE === "private") return;
     if (!isOwner && isGroup && config.MODE === "inbox") return;
