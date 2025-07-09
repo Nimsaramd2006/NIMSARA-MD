5c7d101222ee3b2613f75cb422bb597a19df5c07
Nimsaramd2006 <Nimsaramd123@gmail.com>
2025-07-09 21:25:52 +0530

    Update hack.js

--- plugins/hack.js
+++ plugins/hack.js
@@ -6,7 +6,7 @@ cmd({
     pattern: "hack",
     desc: "Displays a dynamic and playful 'Hacking' message for fun.",
     category: "fun",
-    react: "👨‍💻",
+    react: "💀",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
