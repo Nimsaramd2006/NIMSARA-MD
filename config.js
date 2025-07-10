5bbf6a58a0665ff70c2a12df57a99be57b037f5e
Nimsaramd2006 <Nimsaramd123@gmail.com>
2025-07-10 21:34:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "LxxGgSjZ#Gp_pIoSvjv22Yy_Ej1W_ae32_CjW8qByxf6axKLCMMw",
+SESSION_ID: process.env.SESSION_ID || "LkRTQajA#ViWvk1tlmGKW4G3yFAQSPWe5fkQBiWcHIF2aMPw0nLU",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
@@ -28,7 +28,7 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/SX5N81P0/SulaMd.jpg",
 ALIVE_VIDEO: process.env.ALIVE_VIDEO || "https://files.catbox.moe/wtfov8.mp4",
 MENU_IMG: process.env.MENU_IMG || "https://i.ibb.co/5x5pG4X3/SulaMd.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || `👋 *HI* ${pushname} 😎\n\n✅ *Hello, I am Alive Now!👻*\n\n*╭─「 ꜱᴛᴀᴛᴜꜱ ᴅᴇᴛᴀɪʟꜱ 」*\n| 🤖 Bot :- 𝐍𝐈𝐌𝐒𝛥𝐑𝛥 〽️𝐃 \n| 👤 Owner :- R.Rajindu Nimsara \n| ☎️ Contact :- +94766351670 \n| ✒️ Prefix :- . \n| 🧬 Version :- 1.0.5 \n*╰──────────●●►*\n\n💬 ᴇɴᴏʏ ᴛʜᴇ 𝗯𝗼𝘁 ᴏ𝗳 © NIMSARA-MD❤️🩹 ✨\n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n🥰💚🍒𝐍𝐈𝐌𝐒𝐀𝐑𝐀 𝐌𝐃 𝐖𝐇 𝐁𝐎𝐓🍒💚🥰\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n> 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐛𝐲 𝐌𝐑.𝐑𝐚𝐣𝐢𝐧𝐝𝐮 𝐍𝐢𝐦𝐬𝐚𝐫𝐚\n> ㋛ 𝐏𝐎𝐖𝐄𝐑𝐃 𝐁𝐘 𝐍𝐈𝐌𝐒𝛥𝐑𝛥 〽️𝐃`,
+ALIVE_MSG: process.env.ALIVE_MSG || " *Hello Dear, I am Alive Now!👻*\n\n*╭─「 ꜱᴛᴀᴛᴜꜱ ᴅᴇᴛᴀɪʟꜱ 」*\n| 🤖 Bot :- 𝐍𝐈𝐌𝐒𝛥𝐑𝛥 〽️𝐃 \n| 👤 Owner :- R.Rajindu Nimsara \n| ☎️ Contact :- +94766351670 \n| ✒️ Prefix :- . \n| 🧬 Version :- 1.0.5 \n*╰──────────●●►*\n\n💬 ᴇɴᴏʏ ᴛʜᴇ 𝗯𝗼𝘁 ᴏ𝗳 © NIMSARA-MD❤️🩹 ✨\n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n🥰💚🍒𝐍𝐈𝐌𝐒𝐀𝐑𝐀 𝐌𝐃 𝐖𝐇 𝐁𝐎𝐓🍒💚🥰\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n> 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐛𝐲 𝐌𝐑.𝐑𝐚𝐣𝐢𝐧𝐝𝐮 𝐍𝐢𝐦𝐬𝐚𝐫𝐚\n> ㋛ 𝐏𝐎𝐖𝐄𝐑𝐃 𝐁𝐘 𝐍𝐈𝐌𝐒𝛥𝐑𝛥 〽️𝐃",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
